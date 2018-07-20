09bbfa24be98e45c9a2a30055b7d66e3c322ee01
Thanos <thanos@afrogleap.com>
2018-07-20 15:23:02 +0200

    fixed string bug

--- src/server.js
+++ src/server.js
@@ -32,7 +32,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
 const app = express();
 
 // create graphiql application
-app.use('./graphiql', graphiqlExpress({ endpointURL: './graphql' }));
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
 // connect schemas to graphql
 app.use('/graphql', graphqlExpress({
