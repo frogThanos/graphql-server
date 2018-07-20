ff3280efa075c6d623bf43b3b763c4bd07ebab8b
Thanos <thanos@afrogleap.com>
2018-07-20 15:53:36 +0200

    created addRecipes Mutation

--- src/resolvers.js
+++ src/resolvers.js
@@ -3,5 +3,19 @@ exports.resolvers = {
     getAllRecipes: () => {
 
     }
+  },
+  Mutation: {
+    addRecipe: async (root, args, context) => {
+      const { name, description, category, instructions, username } = args;
+      const { Recipe } = context;
+
+      return newRecipe = await new Recipe({
+        name,
+        description,
+        category,
+        instructions,
+        username,
+      }).save();
+    }
   }
 };
