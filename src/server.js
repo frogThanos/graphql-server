18325ce3456a4dff4579291fccf537742233c50b
Thanos <thanos@afrogleap.com>
2018-07-20 10:00:35 +0200

    created Recipe model

--- src/server.js
+++ src/server.js
@@ -1,1 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config({ path: 'variables.env'});
 
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .then(() => {
+    console.log('successfully connected to database!');
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+const app = express();
+const PORT = process.env.PORT || 4040;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening at ${PORT}`);
+});
