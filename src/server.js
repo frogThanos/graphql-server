c0e055f647ef2a42ceb4622ff2294a4382669cdc
Thanos <thanos@afrogleap.com>
2018-07-20 10:39:00 +0200

    created schema, resolvers files and connected them to graphql

--- src/server.js
+++ src/server.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config({ path: 'variables.env'});
+const bodyParser = require('body-parser');
+
+// models
+const User = require('./models/User');
+const Recipe = require('./models/Recipe');
+
+// graphql express middleware
+const { graphiqlExpress, graphqlExpress } = require('apollo-server-express');
+const { makeExecutableSchema } = require('graphql-tools');
+
+const { typeDefs } = require('./schema');
+const { resolvers } = require('./resolvers');
+
+// create schema
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
+
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
   .then(() => {
@@ -11,6 +30,19 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
   });
 
 const app = express();
+
+// create graphiql application
+app.use('./graphiql', graphiqlExpress({ endpointURL: './graphql' }));
+
+// connect schemas to graphql
+app.use('/graphql', graphqlExpress({
+  schema,
+  context: {
+    Recipe,
+    User,
+  },
+}));
+
 const PORT = process.env.PORT || 4040;
 
 app.listen(PORT, () => {
